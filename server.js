0e3549dcbf2110b12bdca65b67b55d7d0fa01bfb
Pratham Jha <prathamjha5683@gmail.com>
2022-01-30 19:24:12 +0530

    changes

--- server.js
+++ server.js
@@ -55,7 +55,10 @@ if(process.env.NODE_ENV === 'production'){
         res.sendFile(path.resolve(__dirname,'client','build','index.html'));
     })
 }
-
+app.get('/',(req,res)=>{
+    res.send("HEllo WOrld");
+})
 app.listen(Port,()=>{
     console.log("Server is running on port 3001");
+
 })
